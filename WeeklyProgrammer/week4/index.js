515257de06f09439a92ee92a9fb18ec3bfa1b895
Yamikuronue <yamikuronue@gmail.com>
2018-02-12 09:42:34 -0500

    hook up console

--- WeeklyProgrammer/week4/index.js
+++ WeeklyProgrammer/week4/index.js
@@ -2,5 +2,6 @@ const lib = require('./src/index');
 const argv = require('yargs')
 				.demandCommand(1)
 				.argv;
-				
-console.log(lib.findRecurrance(argv._[0]));
+
+const input = argv._[0];
+console.log(`First reoccurance: ${lib.findRecurrance(input)}.  Longest gap: ${lib.findGap(input)}`);
