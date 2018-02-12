68de1f098009ca0dd9ffd41b2bb2a7e272aabeb2
Yamikuronue <yamikuronue@gmail.com>
2018-02-12 09:40:58 -0500

    Solution for week 4 bonus challenge.
    I hates it. Is so inelegent.

--- WeeklyProgrammer/week4/src/index.js
+++ WeeklyProgrammer/week4/src/index.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const stringProcessor = {
 	findRecurrance: (input) => {
 		const letters = [];
@@ -6,6 +8,44 @@ const stringProcessor = {
 			letters.push(letter);
 		}
 		return '';
+	},
+
+	findGap: (input) => {
+		const letters = {};
+
+		//Get indexes
+		for (let i = 0; i < input.length; i++) {
+			let letter = input[i];
+
+			if(!Object.keys(letters).includes(letter)) {
+				letters[letter] = {
+					letter: letter,
+					instances: [],
+					gap: 0
+				};
+			}
+			letters[letter].instances.push(i);
+			
+		}
+	
+		//Reduce
+		_.keys(letters).forEach((item) => {
+			let value = letters[item].instances;
+			let maxGap = value.reduce((val, item, index) => {
+				//calculate current gap
+				let gap = item - (value[index-1]);
+				if (gap > val) {
+					return gap;
+				} else {
+					return val;
+				}
+			}, 0);
+			letters[item].gap = maxGap;
+		})
+
+		const winner = _.orderBy(letters, (item) => item.gap, 'desc')[0];
+		if (winner.gap <= 0) return '';
+		return winner.letter;
 	}
 }
 
--- WeeklyProgrammer/week4/test/indexTest.js
+++ WeeklyProgrammer/week4/test/indexTest.js
@@ -11,6 +11,7 @@ describe('String processing module', () => {
 		it('should return a string', () => oot.findRecurrance('abcd').should.be.a('string'));
 		it('should return empty string when there is no recurrance', () => oot.findRecurrance('abcd').should.equal(''));
 		it('should return a recurring character', () => oot.findRecurrance('abcda').should.equal('a'));
+		it('should return the first recurring character', () => oot.findRecurrance('abcdab').should.equal('a'));
 
 		//Challenge inputs
 		it('should handle challenge input 1', () => oot.findRecurrance('the quick brown fox jumps over the lazy dog').should.equal(' '));
@@ -19,5 +20,23 @@ describe('String processing module', () => {
 		it('should handle challenge input 4', () => oot.findRecurrance('*l1J?)yn%R[}9~1"=k7]9;0[$').should.equal('1'));
 		it('should handle challenge input 5', () => oot.findRecurrance('156248267293222').should.equal('2'));
 		it('should handle challenge input 6', () => oot.findRecurrance('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz').should.equal(''));
+	});
+
+	describe('findGap', () => {
+		it('should exist', () => oot.findGap.should.be.a('function'));
+		it('should return a string', () => oot.findGap('abcd').should.be.a('string'));
+
+		it('should return empty string when there is no recurrance', () => oot.findGap('abcd').should.equal(''));
+		it('should return a gap character', () => oot.findGap('abcda').should.equal('a'));
+		it('should return the longest gapped character', () => oot.findGap('abcbda').should.equal('a'));
+
+		//Challenge inputs
+		
+		it('should handle challenge input 1', () => oot.findGap('the quick brown fox jumps over the lazy dog').should.equal('t'));
+		it('should handle challenge input 2', () => oot.findGap('IKEUNFUVFV').should.equal('U'));
+		it('should handle challenge input 3', () => oot.findGap('PXLJOUDJVZGQHLBHGXIW').should.equal('X'));
+		it('should handle challenge input 4', () => oot.findGap('*l1J?)yn%R[}9~1"=k7]9;0[$').should.equal('['));
+		it('should handle challenge input 5', () => oot.findGap('156248267293222').should.equal('6'));
+		it('should handle challenge input 6', () => oot.findGap('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz').should.equal(''));
 	});
 });
